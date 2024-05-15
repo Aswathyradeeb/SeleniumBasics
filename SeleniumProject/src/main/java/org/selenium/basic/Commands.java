4b3cc82bccf56fd9b6206497d019cf92eaa2adf0
hp <hp@Arish.Home>
2024-05-15 21:26:12 +0100

    commands isDisplayed,isEnabled

--- SeleniumProject/src/main/java/org/selenium/basic/Commands.java
+++ SeleniumProject/src/main/java/org/selenium/basic/Commands.java
@@ -37,27 +37,63 @@ public class Commands {
 		confirmPasswordField.sendKeys("Qa123456789!");
 		WebElement regButton= driver.findElement(By.id("register-button"));
 		regButton.click();	
+		
 	}
 	public void verifyDemowebshopLogin() {
 		WebDriver driver=new ChromeDriver();
-		//driver.get("https://demowebshop.tricentis.com/login");
-		//driver.manage().window().maximize();
-		WebElement loginButton= driver.findElement(By.className("ico-login"));
+		driver.get("https://demowebshop.tricentis.com/login");
+		driver.manage().window().maximize();
+		WebElement loginButton= driver.findElement(By.cssSelector("#Email"));
 		loginButton.click();
 		WebElement emailField=driver.findElement(By.id("Email"));
 		emailField.sendKeys("tom@gmail.com");
 		WebElement passwordField=driver.findElement(By.id("Password"));
 		passwordField.sendKeys("Qa123456789!");
-		WebElement loginButton1= driver.findElement(By.className("button-1 login-button"));
+		WebElement loginButton1= driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input"));
 		loginButton1.click();	
 	}
+	public void verifyDemowebshopIsSelected() {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/register");
+		driver.manage().window().maximize();
+		WebElement genderField=driver.findElement(By.xpath("//*[@id=\"gender-male\"]"));
+		boolean isSelected=genderField.isSelected();
+		System.out.println("Before selected: "+isSelected);
+	    genderField.click();
+	    isSelected=genderField.isSelected();
+	    System.out.println("After selected: "+isSelected);
+	}
+	public void verifyDemowebshopIsEnabled() {
+	    WebDriver driver= new ChromeDriver();
+	    driver.get("https://demowebshop.tricentis.com/");
+	    driver.manage().window().maximize();
+	    WebElement subscribeButton= driver.findElement(By.id("newsletter-subscribe-button"));
+	    Boolean isEnabled= subscribeButton.isEnabled();
+	    System.out.println("Enabled :"+isEnabled);
+	    if(!isEnabled) {
+	    	subscribeButton.click();
+	    	isEnabled= subscribeButton.isEnabled();
+		    System.out.println("Enabled :"+isEnabled);
+	    }    
+	}
+	
+	public void verifyDemowebshopIsDisplayed() {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.manage().window().maximize();
+		WebElement voteButton=driver.findElement(By.id("vote-poll-1"));
+		Boolean isDisplayed=voteButton.isDisplayed();
+		System.out.println("Displayed :"+isDisplayed);
+		driver.close();
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Commands obj=new Commands();
-		obj.verifyDemowebshopRegister();
-		obj.verifyDemowebshopLogin();
+		//obj.verifyDemowebshopRegister();
+		//obj.verifyDemowebshopLogin();
 		//obj.verifySwagLabsLogin();
-		
+		//obj.verifyDemowebshopIsSelected();
+		//obj.verifyDemowebshopIsEnabled();
+		obj.verifyDemowebshopIsDisplayed();
 	}
 
 }
